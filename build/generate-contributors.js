eacd1bc05c68a72524b166f9920607bac0e3f855
plainheart <yhen@all-my-life.cn>
2021-11-07 23:54:43 +0800

    update

--- build/generate-contributors.js
+++ build/generate-contributors.js
@@ -93,7 +93,7 @@ async function ghWeb() {
           let contributors
           const avatars = $('a.avatar-link .avatar-user')
           if (avatars.length) {
-            contributors = avatars.map((i, avatar) => avatar.attr('alt').slice(1))
+            contributors = avatars.map((i, avatar) => $(avatar).attr('alt').slice(1))
           } else {
             contributors = [$('a[rel=contributor] .avatar-user').attr('alt').slice(1)]
           }
@@ -106,6 +106,7 @@ async function ghWeb() {
     })(i)
   }
   await Promise.allSettled(tasks)
+  console.log(entries)
   writeToFile()
 }
 
